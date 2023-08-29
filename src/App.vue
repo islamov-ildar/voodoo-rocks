17d3c5f947f65137815216a8b741b33482ff5bf3
islamov-ildar <39472576+islamov-ildar@users.noreply.github.com>
2023-08-29 11:17:55 +0300

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="input-wrapper">
+    <div class="p-inputgroup flex-1">
+    <span class="p-inputgroup-addon">
+        <i class="pi pi-search"></i>
+    </span>
+      <InputText placeholder="Enter Author name" v-model="searchValue" @input="runSearch" />
+    </div>
+  </div>
+  <div class="card-container">
+    <Card v-for="(onePost, idx) in allPosts" :key="idx" class="post-card">
+      <template #title> <p class="capitalize">{{ onePost.title }} </p></template>
+      <template #content>
+        <p class="capitalize">
+          {{ onePost.body}}
+        </p>
+        <p class="user-name">
+          <strong>{{ onePost.userName}}</strong>
+        </p>
+      </template>
+    </Card>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {ref} from "vue";
+import Card from 'primevue/card';
+import InputText from 'primevue/inputtext';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Card, InputText},
+
+   setup() {
+    const searchValue = ref('');
+    const allPosts = ref();
+    const allUsers = ref();
+
+    const matchingPostAndUsers = () => {
+      allPosts.value.map(onePost => {
+        const currentPostUser = allUsers.value.filter(oneUser => oneUser.id === onePost.userId);
+        onePost.userName = currentPostUser[0].name;
+
+        return onePost;
+      })
+
+    }
+    const getAllUsers = async() =>{
+      const result = await fetch('https://jsonplaceholder.typicode.com/users')
+      allUsers.value = await result.json()
+
+      matchingPostAndUsers();
+    }
+    const getAllPosts = async() =>{
+      const result = await fetch('https://jsonplaceholder.typicode.com/posts')
+      allPosts.value = await result.json()
+
+      await getAllUsers();
+    }
+
+    getAllPosts();
+
+     const runSearch = () => {
+       const cardUserNames = document.querySelectorAll('.user-name');
+       setTimeout(() => {
+         for (let i = 0; i < cardUserNames.length; i++) {
+           if (!cardUserNames[i].textContent.toLowerCase()
+               .includes(searchValue.value.toLowerCase())) {
+             cardUserNames[i].closest(".post-card").classList.add('is-hidden');
+           } else {
+             cardUserNames[i].closest(".post-card").classList.remove('is-hidden')
+           }
+         }
+       }, 300);
+     };
+
+    return {
+      allPosts,
+      searchValue,
+      runSearch,
+    }
+   }
 }
 </script>
 
 <style>
+body {
+  background-color: #4682B4;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +94,27 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.post-card {
+  max-width: 300px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.capitalize:first-letter {
+  text-transform: capitalize;
+}
+.input-wrapper {
+  max-width: 60%;
+  margin: 0 auto 40px auto;
+}
+.is-hidden {
+  display: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import 'primeicons/primeicons.css';
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue).mount('#app')
